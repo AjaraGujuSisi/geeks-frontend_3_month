4ab6c60b4fb1e1894b137985df6690c023462b82
AjaraGujuSisi <154533308+AjaraGujuSisi@users.noreply.github.com>
2024-09-08 21:47:57 +0500

    HomeWork2

--- project/js/home_works.js
+++ project/js/home_works.js
@@ -13,13 +13,87 @@ gmailButton.onclick = () => {
 
 
 const childBlock = document.querySelector(".child_block");
-const parentWidth = 449;
-let position = 0;
+const parentBlock = document.querySelector(".parent_block");
+const parentWidth = parentBlock.clientWidth - childBlock.clientWidth;
+const parentHeight = parentBlock.clientHeight - childBlock.clientHeight;
+let positionX = 0;
+let positionY = 0;
+let direction = 'right';
+const speed = 1;
+
 const moveBlock = () => {
-    if (position < parentWidth) {
-        position++;
-        childBlock.style.left = position + "px";
-        requestAnimationFrame(moveBlock);
+    switch (direction) {
+        case 'right':
+            positionX += speed;
+            if (positionX >= parentWidth) {
+                positionX = parentWidth;
+                direction = 'down';
+            }
+            break;
+        case 'down':
+            positionY += speed;
+            if (positionY >= parentHeight) {
+                positionY = parentHeight;
+                direction = 'left';
+            }
+            break;
+        case 'left':
+            positionX -= speed;
+            if (positionX <= 0) {
+                positionX = 0;
+                direction = 'up';
+            }
+            break;
+        case 'up':
+            positionY -= speed;
+            if (positionY <= 0) {
+                positionY = 0;
+                direction = 'right';
+            }
+            break;
+    }
+
+    childBlock.style.left = `${positionX}px`;
+    childBlock.style.top = `${positionY}px`;
+
+    requestAnimationFrame(moveBlock);
+};
+
+moveBlock();
+
+
+let seconds = 0;
+let timer = null;
+
+const display = document.getElementById('seconds');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+
+const startTimer = () => {
+    if (!timer) {
+        timer = setInterval(() => {
+            seconds++;
+            display.textContent = seconds;
+        }, 1000);
     }
 };
-moveBlock();
+
+
+const stopTimer = () => {
+    clearInterval(timer);
+    timer = null;
+};
+
+
+const resetTimer = () => {
+    stopTimer();
+    seconds = 0;
+    display.textContent = seconds;
+};
+
+// Обработчики событий для кнопок
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+resetButton.addEventListener('click', resetTimer);
+
